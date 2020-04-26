be9b740226a6159dbcd62776d4e38ce313d1e5de
bor <375445894910vitaly@gmail.com>
2020-04-26 15:04:09 +0300

    Create project structure

--- rppba/api/urls.py
+++ rppba/api/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from .docs import urlpatterns as docs_urlpatterns
 
 app_name = 'api'
-urlpatterns = [path('docs/', include(docs_urlpatterns))]
+urlpatterns = [
+    path('docs/', include(docs_urlpatterns)),
+]
