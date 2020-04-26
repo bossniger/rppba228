3c656465d161deb766c96c8b6e274d0c195159ce
bor <375445894910vitaly@gmail.com>
2020-04-26 21:29:43 +0300

    migrations 1

--- rppba/apps/admin.py
+++ rppba/apps/admin.py
@@ -1,19 +1,9 @@
 from django.contrib import admin
-
 # Register your models here.
+from django.contrib.auth import get_user_model
 from django.contrib.admin import AdminSite
 from .product.models import Product, Component, Material
-# from .user.models import User
 
-
-class RppbaAdminSite(AdminSite):
-    site_header = 'Rppba Admin'
-    site_title = 'Rppba Admin Portal'
-
-
-rppba_admin = RppbaAdminSite(name='rppba_admin')
-rppba_admin.register(Material)
-rppba_admin.register(Component)
-rppba_admin.register(Product)
-# rppba_admin.register(User)
-
+admin.site.register(Material)
+admin.site.register(Component)
+admin.site.register(Product)
--- rppba/config/settings.py
+++ rppba/config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'apps',
     'drf_yasg',
 ]
 
@@ -52,6 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+# AUTH_USER_MODEL = 'apps.User'
 
 TEMPLATES = [
     {
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rppba',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Qdwfeg123'
     }
 }
 
