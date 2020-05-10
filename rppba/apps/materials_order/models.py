68bda447d813271ebe1e702d32c21fc44d9742e7
user <vitali.barysenka@leverx.com>
2020-05-10 21:18:29 +0300

    Materials models

--- rppba/apps/materials_order/models.py
+++ rppba/apps/materials_order/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.warehouse_materials.models import WarehouseMaterials
 from users.models import User
 
 
 class MaterialOrder(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    warehouse_material = models.ForeignKey(WarehouseMaterials, on_delete=models.CASCADE)
     quantity = models.IntegerField()
