32b1c1bf34bd4dd35a8245f994008bf91a757728
user <vitali.barysenka@leverx.com>
2020-05-11 04:18:47 +0300

    New permissions + migrations

--- rppba/apps/elements/permissions.py
+++ rppba/apps/elements/permissions.py
@@ -4,5 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsTechnologist(BasePermission):
 
     def has_permission(self, request, view):
-        print('request.user.role')
         return request.user.role == 'technologist'
